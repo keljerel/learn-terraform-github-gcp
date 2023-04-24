e42886ccc5eec0369209ac868b5dcbb6543cb4c8
rdu6390_hca <kelton.buxton@hcahealthcare.com>
2023-04-24 10:46:24 -0500

    adding gcp module

--- main.tf
+++ main.tf
@@ -4,22 +4,12 @@
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
-
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "e2-micro"
-  tags         = ["web", "dev"]
-
-  boot_disk {
-    initialize_params {
-      image = "cos-cloud/cos-stable"
-    }
-  }
-
-  network_interface {
-    network = google_compute_network.vpc_network.self_link
-    access_config {
-    }
-  }
+module "vm_compute_instance" {
+  source  = "terraform-google-modules/vm/google//modules/compute_instance"
+  version = "8.0.0"
+  # insert the 1 required variable here
+  instance_template = "my-first-template"
+  hostname          = "module_instance"
+  network           = "terraform-network"
 }
 
