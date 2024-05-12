aa7a6c593fed363808e69cb64df1342040f99807
Kelton <keljerel@gmail.com>
2024-05-11 19:33:32 -0500

    Deleting GCP instance.

--- main.tf
+++ main.tf
@@ -1,37 +1,37 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
+# # Copyright (c) HashiCorp, Inc.
+# # SPDX-License-Identifier: MPL-2.0
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
-}
+# resource "google_compute_network" "vpc_network" {
+#   name = "terraform-network"
+# }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "e2-micro"
-  labels       = {os = "linux"}
-  tags         = ["ssh"]
+# resource "google_compute_instance" "vm_instance" {
+#   name         = "terraform-instance"
+#   machine_type = "e2-micro"
+#   labels       = {os = "linux"}
+#   tags         = ["ssh"]
 
-  boot_disk {
-    initialize_params {
-      image = "cos-cloud/cos-stable"
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       image = "cos-cloud/cos-stable"
+#     }
+#   }
 
-  network_interface {
-    network = google_compute_network.vpc_network.self_link
-    access_config {
-    }
-  }
-}
-resource "google_compute_firewall" "ssh" {
-  name = "allow-ssh"
-  allow {
-    ports    = ["22"]
-    protocol = "tcp"
-  }
-  direction     = "INGRESS"
-  network       = google_compute_network.vpc_network.id
-  priority      = 1000
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["ssh"]
-}
+#   network_interface {
+#     network = google_compute_network.vpc_network.self_link
+#     access_config {
+#     }
+#   }
+# }
+# resource "google_compute_firewall" "ssh" {
+#   name = "allow-ssh"
+#   allow {
+#     ports    = ["22"]
+#     protocol = "tcp"
+#   }
+#   direction     = "INGRESS"
+#   network       = google_compute_network.vpc_network.id
+#   priority      = 1000
+#   source_ranges = ["0.0.0.0/0"]
+#   target_tags   = ["ssh"]
+# }
